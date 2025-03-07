5af6febf48e17b5686b0f56304cd0bfef6c2ee57
lvidall <lvidaldocarmo@gmail.com>
2025-03-07 20:41:24 -0300

    Aula03_tarefa.py

---
+++ aula03_tarefa01.py
@@ -0,0 +1,8 @@
+nome = "python"
+frase='isso é uma string!'
+multilinha = """isso éuma string de múltipla linhas!"""
+print(type(nome))
+verdadeiro = True 
+falso = False
+print(type(verdadeiro))
+
---
+++ aula03_tarefa02.py
@@ -0,0 +1,26 @@
+a=input("digite o primeiro numero : ")
+a=float(a)
+
+b=input("digite o segundo numero : ") 
+b=float(b)
+
+soma=a+b
+sub=a-b
+multiplicação=a*b
+divisão_real=a/b
+divisão_inteira=a//b
+módulo=a%b
+exponenciação=a**b
+#print("o resultado da soma é")
+#print(soma)
+
+##
+# Metode pt_BR
+##
+
+print(f"o resultado da soma é {soma}")
+
+print(f"o resultado da sub é {sub}")
+print(f"o resultado da multiplcação é{multiplicação}")
+print(f"o resultado da divisão_real é {divisão_real}")
+print(f"o resultado da divisão_inteira é {divisão_inteira}")
