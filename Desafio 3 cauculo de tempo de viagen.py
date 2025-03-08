8922046f3707efa8d3e350b3621a11f643d9f776
lvidall <lvidaldocarmo@gmail.com>
2025-03-07 21:11:45 -0300

    Add files via upload
    desafio 3

---
+++ Desafio 3 cauculo de tempo de viagen.py
@@ -0,0 +1,7 @@
+distancia=float(input("digite a distancia  em km entre o ponto de partida e o ponto de chegada: "))
+velocidade_media=float(input("digite a velocidade media em km percorida no trajeto"))
+divisão_real=distancia/velocidade_media
+print(f"otempo medio de viajem é igual a : {divisão_real} horas")
+
+
+
