a3361e76b84fc2f27be163fe25546c5cc08d3c80
lvidall <lvidaldocarmo@gmail.com>
2025-03-07 21:43:38 -0300

    Add files via upload

---
+++ Aula04 tarefa 1.py
@@ -0,0 +1,16 @@
+x=10
+print(f"o valor inicial de x é {x}")
+x+=5
+print(f"o novo valor de x é{x}")
+x-=3
+print(f"o novo valor de x é{x}")
+x*=2
+print(f"o novo valor de x é{x}")
+x/=4
+print(f"o novo valor de x é {x}")
+x//=3
+print(f"o novo valor de x é{x}")
+x%=2
+print(f"o novo valor de x é {x}")
+x**=3
+print(f"o novo valor de x é {x}")
