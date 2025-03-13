06f05753251328348cb0b50128fe21e17815acaf
lvidall <lvidaldocarmo@gmail.com>
2025-03-12 21:23:34 -0300

    Add files via upload

---
+++ aula05_desafios.05.py
@@ -0,0 +1,10 @@
+SENHA_CORETA="1234"
+
+idade=int(input("informe a sua senha : "))
+if(idade>=18):
+    senha=input("informe a senha de acesso :")
+    if(senha==SENHA_CORRETA):
+    else:
+        print("!!! ACESO NEGADO - SENHA !!!")
+else:
+    print(" !!! ACESSO NEGADO - IDADE !!! ")
