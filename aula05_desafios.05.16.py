611d31eee7639102315b3c518efa88c6e770eedf
lvidall <lvidaldocarmo@gmail.com>
2025-03-14 20:32:48 -0300

    Add files via upload

---
+++ aula05_desafios.05.16.py
@@ -0,0 +1,7 @@
+idade=int(input("digite sua idade:"))
+renda=float(input("qual é sua renda:"))
+nome_sujo=input(" seu nome está sujo:")
+if (idade>21) and (renda>2000) and (nome_sujo=="n"):
+    print("voé pode pegar o emprestimo")
+else:
+
