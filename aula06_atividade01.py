d6742be29c6be349ceb2af1f6d7ce172096cb394
lvidall <lvidaldocarmo@gmail.com>
2025-03-14 21:06:10 -0300

    Add files via upload

---
+++ aula06_atividade01.py
@@ -0,0 +1,11 @@
+frutas = ("maçã", "banana", "cereja")
+for fruta in frutas:
+   print(f"\t{fruta.upper()}") 
+   input("\n PRESIONE ENTER!!!\n")
+
+
+
+   contagen=[9,8,7,6,5,4,3,2,1,0]
+   for i in range(1000):
+    print(i)
+
