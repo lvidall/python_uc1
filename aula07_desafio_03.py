afb75a02069f6c54b6612e5c742ef549db36b99d
lvidall <lvidaldocarmo@gmail.com>
2025-03-19 21:13:00 -0300

    Add files via upload

---
+++ aula07_desafio_03.py
@@ -0,0 +1,21 @@
+para contador de 1 ate 5 faça 
+ contador <- ler numero
+  se cont = 1 
+   maior = numero
+
+  se numero > maior 
+      maior + numero 
+
+imprime (maio)
+
+-------------------------
+
+para contador de 1 ate 5 faça 
+ contador <- ler numero
+  se cont = 1 
+   maior = numero
+senao
+  se numero > maior 
+      maior + numero 
+
+imprime (maio)
