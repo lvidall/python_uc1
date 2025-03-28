7f63140c93780a185a3818b604f46e06de9f1e05
lvidall <lvidaldocarmo@gmail.com>
2025-03-28 17:59:02 -0300

    Add files via upload

---
+++ exercicios_01.py
@@ -0,0 +1,8 @@
+Domínio de linguagens como JavaScript, HTML, CSS, C#, C++, Python e Java.
+	Conhecimento sobre arquitetura web e estrutura end-to-end.
+	Experiência com metodologias ágeis e ferramentas de versionamento como Git.
+
+Três vagas:
+REVVO Brasil 
+Fadami Soluções
+Sofis Tecnologia
