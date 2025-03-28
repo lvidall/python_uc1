b949738275f45e1047809a24ba042f1885ef06d2
lvidall <lvidaldocarmo@gmail.com>
2025-03-28 18:51:09 -0300

    Add files via upload

---
+++ aula10_atividade.01.py
@@ -0,0 +1,21 @@
+vetor = [1, 2, 3, 4, 5]
+print("vetor:", vetor)
+
+print("primeiro elemento:", vetor[0])
+print("Ultimo elemento:", vetor[-1])
+
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print("Elemento (0,0):", matriz[0][0])
+print("Elemento (2,1):", matriz[2][1])
+
+for linha in matriz:
+    for elemento in linha:
+        print(elemento, end=' ')
+     print()
+     
+     
---
+++ aula10_atividade.02.py
@@ -0,0 +1,15 @@
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print("Elemento (0,0):", matriz[0][0])
+print("Elemento (2,1):", matriz[2][1])
+
+for linha in matriz:
+    print(f"|", end=" ")
+    for elemento in linha:
+       print(elemento, end= ' | ' )
+    print()
+     
+     
