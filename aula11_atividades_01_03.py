9691b7cccddf616bb153c9f45cf24be620079d95
lvidall <lvidaldocarmo@gmail.com>
2025-03-31 19:37:00 -0300

    Add files via upload

---
+++ aula11_atividades_01_03.py
@@ -0,0 +1,10 @@
+frase = "o rato roeu a roupa do rei de roma"
+palavras = frase.split()
+contagem = {}
+
+for palavra in palavras:
+ contagem[palavra] = contagem.get(palavra, 0) + 1
+
+print(contagem)
+
+
